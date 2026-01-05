81ec648f086db2a459626c46977fcd196ff8fe2c
Anurag kumar <vanuragkumardesai@gmail.com>
2026-01-05 16:51:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,29 +13,30 @@ st.set_page_config(
 col_1, col_2, col_3, col_4 = st.columns(4)
 pg = st.navigation(
     [
-        st.Page("page1.py", title="Chat With Bot", icon="🏠"),
-        st.Page("page2.py", title="Leaf Classifier", icon="⚙️"),
-        st.Page("page3.py", title="Real vs Ai image Identifier", icon="📊"),
-        st.Page("page4.py", title="Indian Bird Classifier", icon="📊"),
+        st.Page("page\page1.py", title="Chat With Bot", icon="🏠"),
+        st.Page("page\page2.py", title="Leaf Classifier", icon="⚙️"),
+        st.Page("page\page3.py", title="Real vs Ai image Identifier", icon="📊"),
+        st.Page("page\page4.py", title="Indian Bird Classifier", icon="📊"),
     ]
 )
 pg.run()
 with col_1:
     st.image("https://img.freepik.com/premium-vector/chat-bot-concept-illustration_114360-5522.jpg")
     if st.button("Chat with Bot"):
-        st.switch_page("page1.py")
+        st.switch_page("page\page1.py")
     st.text("This model is currently not working good")
 with col_2:
     st.image("https://www.datocms-assets.com/117510/1722393357-act_native_leaf_diy_classifcation_simple_leaf_shapes.jpg")
     if st.button("Leaf classifier"):
-        st.switch_page("page2.py")
+        st.switch_page("page\page2.py")
 with col_3:
     st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK9-5Jz7JeFvV21D6Hxyj0Q7HsNaYJEOLEGQ&s")
     if st.button("Real vs Ai image identifier"):
-        st.switch_page("page3.py")
+        st.switch_page("page\page3.py")
 with col_4:
     st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREF8dHvPsKqd7F6u8dDjQPEUHZIsMsU-2n2A&s")
     if st.button("Indian bird classifier"):
-        st.switch_page("page4.py")
+        st.switch_page("page\page4.py")
+
 
 
